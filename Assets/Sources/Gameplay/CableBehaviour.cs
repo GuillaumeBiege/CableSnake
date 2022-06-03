f9db3d98dc910227810954e363c52ecd23abf290
GuillaumeBiege <Guillaume.biege@gmail.com>
2022-06-03 15:04:55 +0200

    Some scene organisation and major level design changes

--- Assets/Sources/Gameplay/CableBehaviour.cs
+++ Assets/Sources/Gameplay/CableBehaviour.cs
@@ -11,15 +11,31 @@ public class CableBehaviour : MonoBehaviour
     [SerializeField] float currentspeed = 1f;
     [SerializeField] bool IsMoving = false;
 
+
+    [Header("LevelGeneration")]
+    [SerializeField] GameObject cableSegment = default;
+    [SerializeField] GameObject[] obstacleList = new GameObject[30];
+    [SerializeField] int progressionIndex = 0;
+    [SerializeField] float progressionStep = 10f;
+    [SerializeField] Queue<GameObject> instancedObtacles = default;
+
+    [SerializeField] float progressionTimer = 0f;
+
+
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
 
-        GameManager.Instance.ONGameMode += Init;
+        GameManager.Instance.ONGameMode += LaunchProgression;
+    }
+
+    private void Start()
+    {
+        Init();
     }
 
 
-    public void Init()
+    public void LaunchProgression()
     {
         IsMoving = true;
     }
@@ -29,4 +45,61 @@ public class CableBehaviour : MonoBehaviour
         if (IsMoving)
             transform.position += -transform.forward * currentspeed * Time.deltaTime;
     }
+
+    #region Level generation
+    void Init()
+    {
+        //For the player breathing room at the beggining of a level
+        //AddEmptySegment();
+
+
+        for (int i = 0; i < 6 && i < obstacleList.Length; i++)
+        {
+            AddFullSegment();
+
+
+            
+        }
+    }
+
+    void AddEmptySegment()
+    {
+        GameObject cable = Instantiate<GameObject>(cableSegment, transform);
+        cable.transform.position = new Vector3(0f, 0f, progressionIndex * progressionStep);
+        progressionIndex++;
+    }
+
+    void AddFullSegment()
+    {
+        if (progressionIndex < obstacleList.Length)
+        {
+            GameObject obstacle = Instantiate<GameObject>(obstacleList[progressionIndex], transform);
+            obstacle.transform.position = new Vector3(0f, 0f, progressionIndex * progressionStep);
+
+            AddEmptySegment();
+        }
+    }
+
+
+    private void Update()
+    {
+        if (GameManager.Instance.currentGameState == GameManager.GameState.INGAME)
+        {
+            float segmentTime = progressionStep / currentspeed;
+
+            progressionTimer += Time.deltaTime;
+
+            if (progressionTimer >= segmentTime)
+            {
+                progressionTimer -= segmentTime;
+                
+                
+                AddFullSegment();
+            }
+        }
+        
+    }
+    #endregion
+
+
 }
--- Assets/Sources/Gameplay/FoodBehaviour.cs
+++ Assets/Sources/Gameplay/FoodBehaviour.cs
@@ -6,12 +6,17 @@ public class FoodBehaviour : MonoBehaviour
 {
     //References
     [SerializeField] GameObject deathFX = default;
+    [SerializeField] CableBehaviour cable = default;
 
+    private void Start()
+    {
+        cable = GetComponentInParent<CableBehaviour>();
+    }
 
     public void KillItself()
     {
         //Create FX at death
-        GameObject go = Instantiate<GameObject>(deathFX);
+        GameObject go = Instantiate<GameObject>(deathFX, cable.transform);
         go.transform.position = transform.position;
 
         //Die
